8efeaf40171ed4a01bd0fbf7e81dce241a76cdbd
rakeshkumawat12 <rakeshkumawatrrkk12877@gmail.com>
2025-07-21 19:02:44 +0530

    add mod

---
+++ programs/solana-amm-anchor/src/instructions/mod.rs
@@ -0,0 +1,15 @@
+pub mod initialize;
+pub use initialize::*;
+
+
+pub mod deposit;
+pub use deposit::*;
+
+pub mod update;
+pub use update::*;
+
+pub mod swap;
+pub use swap::*;
+
+pub mod withdraw;
+pub use withdraw::*;
